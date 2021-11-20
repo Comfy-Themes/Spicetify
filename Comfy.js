61f5da6bbcda3b62139140f1eb7481b9de282f36
Nyria <nyriaoff@gmail.com>
2021-11-20 17:03:53 +0100

    Auto-update for CSS & multiple fixes

--- Comfy.js
+++ Comfy.js
@@ -1,37 +1,46 @@
 (function Comfy() {
-    const { Player, Menu, LocalStorage, Platform } = Spicetify;
-    const main = document.querySelector('.Root__main-view');
-    const mainChild = document.createElement("div");
+    const { Player, Menu, LocalStorage, Platform } = Spicetify
+    const main = document.querySelector('.Root__main-view')
+    const mainChild = document.createElement("div")
 
     if (!(Player && Menu && LocalStorage && Platform && main)) {
-        setTimeout(Comfy, 1000);
-        return;
+        setTimeout(Comfy, 1000)
+        return
     }
 
-    const channels = Platform.History.location.pathname.startsWith('/playlist/') || Platform.History.location.pathname.startsWith('/album/') || Platform.History.location.pathname.startsWith('/collection/tracks');
-    main.appendChild(mainChild);
-    mainChild.id = "mainImage";
+    const channels = ['/playlist/', '/album/', '/collection/tracks', '/collection/episodes', '/episode/']
+    main.appendChild(mainChild)
+    mainChild.id = "mainImage"
 
     // On a playlist
-    if (channels) {
-        mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")";
+    for (var i = 0; i < channels.length; i++) {
+
+        if (Platform.History.location.pathname.startsWith(channels[i])) {
+            mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")"
+        }
     }
 
     // Waiting for a switch between channels
     Platform.History.listen(({ pathname }) => {
 
         // If the channel is a playlist
-        if (pathname.startsWith('/playlist/') || pathname.startsWith('/album/') || pathname.startsWith('/collection/tracks')) {
-            mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")";
-        }
+        for (var i = 0; i < channels.length; i++) {
+            
+            if (pathname.startsWith(channels[i])) {
+                mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")"
+                return
+            }
 
-        else { mainChild.style.backgroundImage = null;}
-    });
+            else { mainChild.style.backgroundImage = null}
+        }
+    })
 
     // Change the song image on song change
     Player.addEventListener("songchange", () => {
-        if (channels) {
-            mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")";
+        for (var i = 0; i < channels.length; i++) {
+            if (Platform.History.location.pathname.startsWith(channels[i])) {
+                mainChild.style.backgroundImage = "url(" + Player.data.track.metadata.image_xlarge_url + ")"
+            }
         }
-    });
-})();
+    })
+})()
