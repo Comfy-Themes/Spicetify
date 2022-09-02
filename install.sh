6c92ab7a824c9ff3d74443b4bfba54cabb95d4ac
Lucas <me@lhvy.dev>
2022-09-02 23:56:27 +1000

    Clean up Unix install scripts (#77)
    * Clean up shell scripts
    
    * Add progress bar to install.sh

--- install.sh
+++ install.sh
@@ -2,25 +2,51 @@
 
 set -e
 
+function dots {
+    while true; do
+        printf "."
+        sleep 0.1
+    done
+}
+
 echo "Downloading"
 # Setup directories to download to
-theme_dir="$(dirname "$(spicetify -c)")/Themes"
-ext_dir="$(dirname "$(spicetify -c)")/Extensions"
+spice_dir="$(dirname "$(spicetify -c)")"
+theme_dir="${spice_dir}/Themes"
+ext_dir="${spice_dir}/Extensions"
 
 # Make directories if needed
 mkdir -p "${theme_dir}/Comfy"
 mkdir -p "${ext_dir}"
 
 # Download latest tagged files into correct directories
-curl --progress-bar --output "${theme_dir}/Comfy/color.ini" "https://raw.githubusercontent.com/Comfy-Themes/Spicetify/main/Comfy/color.ini"
-curl --progress-bar --output "${theme_dir}/Comfy/user.css" "https://raw.githubusercontent.com/comfy-themes/Spicetify/main/Comfy/user.css"
-curl --progress-bar --output "${ext_dir}/comfy.js" "https://raw.githubusercontent.com/comfy-themes/Spicetify/main/Comfy/comfy.js"
+theme_url="https://raw.githubusercontent.com/Comfy-Themes/Spicetify/main/Comfy"
 
+# Call dots function in background
+dots &
+dots_pid=$!
+# Avoid kill message for dots
+disown
+
+# Store PIDs of curls to kill later
+pids=()
+curl --silent --output "${theme_dir}/Comfy/color.ini" "${theme_url}/color.ini" &
+pids+=($!)
+curl --silent --output "${theme_dir}/Comfy/user.css" "${theme_url}/user.css" &
+pids+=($!)
+curl --silent --output "${ext_dir}/comfy.js" "${theme_url}/comfy.js" &
+pids+=($!)
+
+# Wait for all curls to finish and kill dots
+for pid in "${pids[@]}"; do
+    wait $pid
+done
+kill $dots_pid
+echo " Done"
 
 # Apply theme
 echo "Applying theme"
-spicetify config extensions comfy.js
-spicetify config current_theme Comfy
+spicetify config current_theme Comfy color_scheme Comfy extensions comfy.js
 spicetify config inject_css 1 replace_colors 1 overwrite_assets 1
 spicetify apply
 
