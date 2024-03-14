ec4b1eeb7b1b24f80c61cd74a61a214f20971133
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-03-14 18:10:24 +0000

    Update Comfy settings and theme script

--- Comfy/theme.script.js
+++ Comfy/theme.script.js
@@ -1,11 +1,10 @@
 /* 
 tofix:
-- migrate carousel style to settings.scss
-- fix carousel css when loading playlist page with no expanded sidebar
-- fix carousel mouse scrolling (drag / scroll)
-- fix carousel keyboard scrolling (arrow keys)
-- fix images on preview tippy
-- fix negative values
+- dropdowns not closing on click outside
+- carousel mouse scrolling (drag / scroll)
+- carousel keyboard scrolling (arrow keys)
+- images on preview tippy
+- negative values
 
 todo:
 - seperate chip logic from carousel logic - maybe use spicetify component for chip
@@ -1566,6 +1565,32 @@ torefactor:
 		);
 	};
 
+	const headerButton = ({ label, link, svg, viewBox }) => {
+		return Spicetify.React.createElement(
+			Spicetify.ReactComponent.TooltipWrapper,
+			{
+				label: label,
+				showDelay: 200
+			},
+			Spicetify.React.createElement(
+				"button",
+				{
+					className: "main-trackCreditsModal-closeBtn",
+					onClick: () => window.open(link)
+				},
+				Spicetify.React.createElement("svg", {
+					width: "18",
+					height: "18",
+					viewBox: viewBox,
+					fill: "currentColor",
+					dangerouslySetInnerHTML: {
+						__html: svg
+					}
+				})
+			)
+		);
+	};
+
 	// Settings Button + Modal
 	waitForDeps("Spicetify.Topbar.Button", () => {
 		new Spicetify.Topbar.Button(
@@ -1583,17 +1608,28 @@ torefactor:
 					isLarge: true
 				});
 
-				// Discord Text
+				// Social Buttons
 				const header = document.querySelector(".main-trackCreditsModal-header");
+				const closeButton = document.querySelector(".main-trackCreditsModal-closeBtn");
 				const container = document.createElement("div");
-				const extraText = document.createElement("a");
-				extraText.textContent = "Need support? Click here!";
-				extraText.href = "https://discord.gg/rtBQX5D3bD";
-				extraText.style.color = "var(--spice-notification)";
+				const socialButtons = [
+					Spicetify.React.createElement(headerButton, {
+						label: "Join our Discord!",
+						link: "https://discord.gg/rtBQX5D3bD",
+						svg: `<g xmlns="http://www.w3.org/2000/svg"><path d="M216.856339,16.5966031 C200.285002,8.84328665 182.566144,3.2084988 164.041564,0 C161.766523,4.11318106 159.108624,9.64549908 157.276099,14.0464379 C137.583995,11.0849896 118.072967,11.0849896 98.7430163,14.0464379 C96.9108417,9.64549908 94.1925838,4.11318106 91.8971895,0 C73.3526068,3.2084988 55.6133949,8.86399117 39.0420583,16.6376612 C5.61752293,67.146514 -3.4433191,116.400813 1.08711069,164.955721 C23.2560196,181.510915 44.7403634,191.567697 65.8621325,198.148576 C71.0772151,190.971126 75.7283628,183.341335 79.7352139,175.300261 C72.104019,172.400575 64.7949724,168.822202 57.8887866,164.667963 C59.7209612,163.310589 61.5131304,161.891452 63.2445898,160.431257 C105.36741,180.133187 151.134928,180.133187 192.754523,160.431257 C194.506336,161.891452 196.298154,163.310589 198.110326,164.667963 C191.183787,168.842556 183.854737,172.420929 176.223542,175.320965 C180.230393,183.341335 184.861538,190.991831 190.096624,198.16893 C211.238746,191.588051 232.743023,181.531619 254.911949,164.955721 C260.227747,108.668201 245.831087,59.8662432 216.856339,16.5966031 Z M85.4738752,135.09489 C72.8290281,135.09489 62.4592217,123.290155 62.4592217,108.914901 C62.4592217,94.5396472 72.607595,82.7145587 85.4738752,82.7145587 C98.3405064,82.7145587 108.709962,94.5189427 108.488529,108.914901 C108.508531,123.290155 98.3405064,135.09489 85.4738752,135.09489 Z M170.525237,135.09489 C157.88039,135.09489 147.510584,123.290155 147.510584,108.914901 C147.510584,94.5396472 157.658606,82.7145587 170.525237,82.7145587 C183.391518,82.7145587 193.761324,94.5189427 193.539891,108.914901 C193.539891,123.290155 183.391518,135.09489 170.525237,135.09489 Z" fill="currentColor" fill-rule="nonzero"></path></g>`,
+						viewBox: "0 -28.5 256 256"
+					}),
+					Spicetify.React.createElement(headerButton, {
+						label: "Visit our GitHub org!",
+						link: "https://github.com/Comfy-Themes",
+						svg: `<path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>`,
+						viewBox: "0 0 16 16"
+					})
+				];
 
-				container.appendChild(document.querySelector("h1.main-type-alto"));
-				container.appendChild(extraText);
-				header.prepend(container);
+				Spicetify.ReactDOM.render(socialButtons, container);
+				container.appendChild(closeButton);
+				header.appendChild(container);
 
 				// Scroll Position
 				const section = document.querySelector(".main-trackCreditsModal-mainSection");
