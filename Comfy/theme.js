4fb89b0cc3c8c3d568181627d6a3e87f32c20681
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-03-15 22:37:16 +0000

    revert change

--- Comfy/theme.js
+++ Comfy/theme.js
@@ -1,6 +1,6 @@
 (() => {
 	const themeScript = document.createElement("SCRIPT");
 	themeScript.setAttribute("type", "text/javascript");
-	themeScript.setAttribute("src", "https://esm.sh/gh/comfy-themes/spicetify@beta/Comfy/theme.script.js");
+	themeScript.setAttribute("src", "https://esm.sh/gh/comfy-themes/spicetify@beta/Comfy/theme.script.js?raw");
 	document.head.appendChild(themeScript);
 })();
