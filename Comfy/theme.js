3c00e3cf5ebf611073991bb7063e63453aadf1da
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-03-15 22:32:18 +0000

    Update theme.js to use the beta version of the theme script

--- Comfy/theme.js
+++ Comfy/theme.js
@@ -1,6 +1,6 @@
 (() => {
 	const themeScript = document.createElement("SCRIPT");
 	themeScript.setAttribute("type", "text/javascript");
-	themeScript.setAttribute("src", "https://esm.sh/gh/comfy-themes/spicetify@b6c94782c5/Comfy/theme.script.js?raw");
+	themeScript.setAttribute("src", "https://esm.sh/gh/comfy-themes/spicetify@beta/Comfy/theme.script.js?raw");
 	document.head.appendChild(themeScript);
 })();
