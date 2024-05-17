fe6903f029e1248c98bd71cfdf9cef28373a14a7
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-05-17 18:29:09 +0100

    chore: Remove unused throttle function in theme.script.js

--- Comfy/theme.script.js
+++ Comfy/theme.script.js
@@ -1704,27 +1704,6 @@ todo:
 		}
 	}
 
-	function throttle(func, limit) {
-		let lastFunc;
-		let lastRan;
-		return function () {
-			const context = this;
-			const args = arguments;
-			if (!lastRan) {
-				func.apply(context, args);
-				lastRan = Date.now();
-			} else {
-				clearTimeout(lastFunc);
-				lastFunc = setTimeout(function () {
-					if (Date.now() - lastRan >= limit) {
-						func.apply(context, args);
-						lastRan = Date.now();
-					}
-				}, limit - (Date.now() - lastRan));
-			}
-		};
-	}
-
 	async function waitForDeps(dependencies, callback, element = false, elementType = "querySelector", timeout = 5000) {
 		return new Promise(resolve => {
 			let allDependenciesLoaded = false;
