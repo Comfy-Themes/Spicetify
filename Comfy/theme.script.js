d877e70dd97ec11d50f6a64f9c7c0b8e511cfbe1
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-07-04 14:04:41 +0100

    chore: Refactor ConfirmDialog component in theme.script.js

--- Comfy/theme.script.js
+++ Comfy/theme.script.js
@@ -150,6 +150,22 @@ todo:
 	const Dialog = Spicetify.React.memo(props => {
 		const [state, setState] = Spicetify.React.useState(true);
 		const self = document.querySelector(".ReactModalPortal:last-of-type");
+		const ConfirmDialog = Spicetify.ReactComponent.ConfirmDialog;
+		const isForwardRef = typeof ConfirmDialog === "function";
+		const commonProps = {
+			...props,
+			isOpen: state,
+			onClose: () => {
+				setState(false);
+				props.onClose?.();
+				self.remove();
+			},
+			onConfirm: () => {
+				setState(false);
+				props.onConfirm?.();
+				self.remove();
+			}
+		};
 
 		Spicetify.React.useEffect(() => {
 			if (state) {
@@ -157,20 +173,7 @@ todo:
 			}
 		}, [state]);
 
-		return Spicetify.ReactComponent.ConfirmDialog({
-			...props,
-			isOpen: state,
-			onClose: () => {
-				setState(false);
-				props.onClose();
-				self.remove();
-			},
-			onConfirm: () => {
-				setState(false);
-				props.onConfirm();
-				self.remove();
-			}
-		});
+		return isForwardRef ? ConfirmDialog(commonProps) : Spicetify.React.createElement(ConfirmDialog, commonProps);
 	});
 
 	const Section = ({ name, children, condition = true, filter }) => {
