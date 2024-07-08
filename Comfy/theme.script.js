d5ddf857843f88a52e82fed38c14de01a7532674
OhItsTom <22730962+ohitstom@users.noreply.github.com>
2024-07-08 18:51:52 +0100

    Revert "chore: Refactor ConfirmDialog component in theme.script.js"
    This reverts commit d877e70dd97ec11d50f6a64f9c7c0b8e511cfbe1.

--- Comfy/theme.script.js
+++ Comfy/theme.script.js
@@ -151,22 +151,6 @@ todo:
 	const Dialog = Spicetify.React.memo(props => {
 		const [state, setState] = Spicetify.React.useState(true);
 		const self = document.querySelector(".ReactModalPortal:last-of-type");
-		const ConfirmDialog = Spicetify.ReactComponent.ConfirmDialog;
-		const isForwardRef = typeof ConfirmDialog === "function";
-		const commonProps = {
-			...props,
-			isOpen: state,
-			onClose: () => {
-				setState(false);
-				props.onClose?.();
-				self.remove();
-			},
-			onConfirm: () => {
-				setState(false);
-				props.onConfirm?.();
-				self.remove();
-			}
-		};
 
 		Spicetify.React.useEffect(() => {
 			if (state) {
@@ -174,7 +158,20 @@ todo:
 			}
 		}, [state]);
 
-		return isForwardRef ? ConfirmDialog(commonProps) : Spicetify.React.createElement(ConfirmDialog, commonProps);
+		return Spicetify.ReactComponent.ConfirmDialog({
+			...props,
+			isOpen: state,
+			onClose: () => {
+				setState(false);
+				props.onClose();
+				self.remove();
+			},
+			onConfirm: () => {
+				setState(false);
+				props.onConfirm();
+				self.remove();
+			}
+		});
 	});
 
 	const Section = ({ name, children, condition = true, filter }) => {
