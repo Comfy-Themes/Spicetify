39d5d0ca28c0c7a64d115d73b5804df1ae471cf4
Mephisto <38382271+NotMephisto@users.noreply.github.com>
2024-11-08 20:19:21 +0300

    Update install.sh
    Fix link

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Download URL
-theme_url="https://raw.githubusercontent.com/Comfy-Themes/Spicetify/main/Comfy"
+https://github.com/Comfy-Themes/Spicetify/tree/db9a174bfe30cd39370ae6a31d854e119617b4aa/Comfy
+theme_url="https://raw.githubusercontent.com/NotMephisto/Comfy-matugen/main/Comfy"
 
 # Setup directories to download to
 spice_dir="$(dirname "$(spicetify -c)")"
